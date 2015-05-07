b52ae2ec8d3ea1d0edf620a2a23edc8572258e1c
Adam Struck <adamjstruck@gmail.com>
2015-05-07 14:29:20 -0700

    updated MisoGffToBed

--- MisoGffToBed_plus_sequences.R
+++ MisoGffToBed_plus_sequences.R
@@ -3,7 +3,7 @@
 ##----------------------------------
 gff_file <- "~/Projects/dmseq/data/event_annotations/ALE.hg19.gff3"
 
-ALE_res <- UTRgffToBed(gff_file)
+ALE_res <- UTRgffToBed(gff_file, event_type = "AFE")
 
 write.table(ALE_res, file = "~/Projects/dmseq/data/ALE_metadata.txt",
             sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
@@ -13,10 +13,20 @@ write.table(ALE_res, file = "~/Projects/dmseq/data/ALE_metadata.txt",
 ##----------------------------------
 gff_file <- "~/Projects/dmseq/data/event_annotations/AFE.hg19.gff3"
 
-AFE_res <- UTRgffToBed(gff_file)
+AFE_res <- MisoGffToBed(gff_file, event_type = "ALE")
 
 write.table(AFE_res, file = "~/Projects/dmseq/data/AFE_metadata.txt",
             sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+
+##----------------------------------
+## Alt polyA
+##----------------------------------
+gff_file <- "~/Projects/dmseq/data/event_annotations/polyAseq.hg19.entrezID.v2.gff3"
+
+polyA_res <- MisoGffToBed(gff_file, event_type = "polyA")
+
+write.table(polyA_res, file = "~/Projects/dmseq/data/nonUTRevents.multi_metadata.txt",
+            col.names = TRUE, row.names = FALSE, sep = "\t", quote = FALSE)
 
 ##----------------------------------
 ## nonUTRevents
@@ -28,55 +38,34 @@ nonUTRevents_res <- MisoGffToBed(gff_file)
 write.table(nonUTRevents_res, file = "~/Projects/dmseq/data/nonUTRevents.multi_metadata.txt",
             col.names = TRUE, row.names = FALSE, sep = "\t", quote = FALSE)
 
-##----------------------------------
+
 ##----------------------------------
 ## Analysis Functions
 ##----------------------------------
-##----------------------------------
-MisoGffToBed <- function(gff_file) {
+MisoGffToBed <- function(gff_file, event_type = "nonUTRevents") {
     require(data.table)
     require(dplyr)
     require(GenomicRanges)
-    
+    ## Read gff file and reformat to BED
     gff <- fread(gff_file)
-
     gff <- gff %>% dplyr::select(V1, V4, V5, V9, V3, V7, V2)
     setnames(gff, c("chr", "start", "end", "isoform", "feature", "strand", "event_type"))
-
     gff_exons <- gff %>% dplyr::filter(feature == "exon")
-
-    gff_exons[, feature := gsub("ID=chr.+[+-].[A-Z].", "", gsub(";Parent=.+", "", isoform))]   
-    gff_exons[, isoform := gsub("ID=.+;", "", isoform)]
-    gff_exons[, isoform := gsub("Parent=", "", isoform)]
-    gff_exons[, event_name := gsub("..$", "", isoform)]
-    gff_exons[, exon_length := end - start + 1]
-
-    gff_exons$seq <- appendSequences(gff_exons)
-    
-    return(gff_exons)
-}
-
-UTRgffToBed <- function(gff_file) {
-    require(data.table)
-    require(dplyr)
-    require(GenomicRanges)
-    
-    gff <- fread(gff_file)
-
-    gff <- gff %>% dplyr::select(V1, V4, V5, V9, V3, V7, V2)
-    setnames(gff, c("chr", "start", "end", "isoform", "feature", "strand", "event_type"))
-
-    gff_exons <- gff %>% dplyr::filter(feature == "exon")
-
-    gff_exons[, isoform := gsub(";Parent=.+", "", isoform)]
-    gff_exons[, isoform := gsub("ID=", "", isoform)]    
-    gff_exons[, gene_symbol := gsub("..$", "", isoform)]
-    gff_exons[, exon_length := end - start + 1]
-
-    gff_exons %<>% group_by(isoform) %>% mutate(isoform_length = sum(exon_length))
-
-    gff_exons$seq <- appendSequences(gff_exons)
-    
+    ## Check event_type
+    if (event_type == "nonUTRevents") {        
+        gff_exons[, feature := gsub("ID=chr.+[+-].[A-Z].", "", gsub(";Parent=.+", "", isoform))]   
+        gff_exons[, isoform := gsub("ID=.+;", "", isoform)]
+        gff_exons[, isoform := gsub("Parent=", "", isoform)]
+        gff_exons[, event_name := gsub("..$", "", isoform)]
+        gff_exons[, exon_length := end - start + 1]
+    } else {
+        gff_exons[, isoform := gsub(";Parent=.+", "", isoform)]
+        gff_exons[, isoform := gsub("ID=", "", isoform)]    
+        gff_exons[, gene_symbol := gsub("..$", "", isoform)]
+        gff_exons[, exon_length := end - start + 1]
+        gff_exons <- gff_exons %>% group_by(isoform) %>% mutate(isoform_length = sum(exon_length))
+    }     
+    gff_exons$seq <- appendSequences(gff_exons)    
     return(gff_exons)
 }
 
